b04e0c84f3e55cf61c1f1bd5a392c03d0325fdde
Vincent van den Braken <vvdbraken@quintor.nl>
2018-04-06 17:04:12 +0200

    Initial Commit of default blazor application with Bootstrap4 added and Qwinkel API proxy + products page

---
+++ qwinkel/qwinkel.Server/Startup.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Blazor.Server;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using System.Linq;
+using System.Net.Mime;
+
+namespace qwinkel.Server
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+            });
+
+            services.AddResponseCompression(options =>
+            {
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
+                {
+                    MediaTypeNames.Application.Octet,
+                    WasmMediaTypeNames.Application.Wasm,
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.MapWhen(IsQwinkelApi, builder => builder.RunProxy(new ProxyOptions
+            {
+                Scheme = "http",
+                Host = "qwinkel.quintor.nl",
+                Port = "80"
+            }));
+
+            app.UseResponseCompression();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "default", template: "{controller}/{action}/{id?}");
+            });
+
+            app.UseBlazor<Client.Program>();
+        }
+
+        private static bool IsQwinkelApi(Microsoft.AspNetCore.Http.HttpContext httpContext)
+        {
+            return httpContext.Request.Path.Value.StartsWith(@"/api/", System.StringComparison.OrdinalIgnoreCase) ||
+                httpContext.Request.Path.Value.StartsWith(@"/afbeeldingen/", System.StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ qwinkel/qwinkel.Shared/qwinkelapi/Producten.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace qwinkel.Shared.Producten
+{
+    public class Afbeelding
+    {
+        public int id { get; set; }
+        public string filePath { get; set; }
+    }
+
+    public class Content
+    {
+        public int id { get; set; }
+        public string naam { get; set; }
+        public string omschrijving { get; set; }
+        public double prijs { get; set; }
+        public double btw { get; set; }
+        public string youtubeUrl { get; set; }
+        public Afbeelding afbeelding { get; set; }
+    }
+
+    public class RootObject
+    {
+        public int totalElements { get; set; }
+        public int totalPages { get; set; }
+        public object sort { get; set; }
+        public bool first { get; set; }
+        public bool last { get; set; }
+        public int numberOfElements { get; set; }
+        public int size { get; set; }
+        public List<Content> content { get; set; }
+        public int number { get; set; }
+    }
+}
